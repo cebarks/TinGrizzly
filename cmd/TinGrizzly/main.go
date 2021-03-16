a0d79b71f1b8b5c3076760aece2317d454c76f71
Anten Skrabec <cebarks@gmail.com>
2021-03-16 15:01:16 -0600

    add missing cmd dir

---
+++ cmd/TinGrizzly/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cebarks/TinGrizzly/internal/game"
+	"github.com/cebarks/TinGrizzly/internal/util"
+
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func main() {
+	log.Println("Launching...")
+	util.SetupLogging()
+	gam := &game.Game{}
+
+	util.Running = true
+	SetupCloseHandler()
+	pixelgl.Run(gam.Run)
+
+}
+
+// SetupCloseHandler creates a 'listener' on a new goroutine which will notify the
+// program if it receives an interrupt from the OS. We then handle this by calling
+// our clean up procedure and exiting the program.
+func SetupCloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Println("SIGINT Received")
+		util.Running = false
+	}()
+}
