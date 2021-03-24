92a54c3e427b839b33b7c2bc3000c182267d1b75
Anten Skrabec <cebarks@gmail.com>
2021-03-24 00:27:46 -0600

    add log file

---
+++ internal/util/logging.go
@@ -0,0 +1,82 @@
+package util
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/snwfdhmp/errlog"
+)
+
+var (
+	errorLogger errlog.Logger
+	// DebugError handles an error with errlog
+	DebugError func(error) bool
+)
+
+//SetupLogging sets up errlog and zerolog and sets errlog to use zerolog to
+func SetupLogging() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	consoleWriter := zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		NoColor:    false,
+		TimeFormat: time.RFC1123Z,
+		FormatCaller: func(i interface{}) string {
+			path := i.(string)
+			idx := strings.Index(path, "/TinGrizzly/") + 12
+			//trim build path from output
+			return path[idx:]
+		},
+	}
+
+	wd, _ := os.Getwd()
+	path := filepath.Join(wd, "log", fmt.Sprintf("log-%s.json", strings.ReplaceAll(time.Now().Format(time.Stamp), " ", "-")))
+
+	os.Mkdir("log", 0755)
+
+	logFile, err := os.Create(path)
+	if err != nil {
+		log.Error().Err(err).Msg("Couldn't create log file.")
+	} else {
+		log.Info().Msgf("Saving log file to: %s", path)
+	}
+
+	fileWriter := bufio.NewWriter(logFile)
+
+	go func() {
+		defer fileWriter.Flush()
+		for {
+			fileWriter.Flush()
+			time.Sleep(25 * time.Millisecond)
+		}
+	}()
+
+	log.Logger = log.Output(zerolog.MultiLevelWriter(consoleWriter, fileWriter))
+
+	errorLogger = errlog.NewLogger(&errlog.Config{
+		LinesBefore:        3,
+		LinesAfter:         4,
+		PrintError:         true,
+		PrintSource:        true,
+		PrintStack:         true,
+		ExitOnDebugSuccess: false,
+		PrintFunc: func(format string, data ...interface{}) {
+			log.Error().Msgf(format, data...)
+		},
+	})
+
+	if zerolog.GlobalLevel() <= zerolog.TraceLevel {
+		//adds file and line number to log
+		log.Logger = log.With().Caller().Logger()
+	} else {
+		errorLogger.Disable(true)
+	}
+
+	DebugError = errorLogger.Debug
+}
--- internal/util/util.go
+++ internal/util/util.go
@@ -4,24 +4,16 @@ import (
 	"image"
 	_ "image/png"
 	"os"
-	"time"
 
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/faiface/pixel"
 	"github.com/kelindar/tile"
-	"github.com/snwfdhmp/errlog"
 )
 
 var (
 	// Running - global variable for app state
-	Running bool
-
-	errorLogger errlog.Logger
-	// DebugError handles an error with errlog
-	DebugError func(error) bool
-
+	Running   bool
 	GitCommit string
 )
 
@@ -30,36 +22,6 @@ func Startup() {
 	log.Info().Str("version", GitCommit).Msg("Launching...")
 	ReloadCfgFromDisk()
 	Running = true
-}
-
-//SetupLogging sets up errlog and zerolog and sets errlog to use zerolog to
-func SetupLogging() {
-	// set pretty console output for zerolog
-	log.Logger = log.Output(zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: time.RFC1123Z,
-	})
-
-	errorLogger = errlog.NewLogger(&errlog.Config{
-		LinesBefore:        3,
-		LinesAfter:         4,
-		PrintError:         true,
-		PrintSource:        true,
-		PrintStack:         true,
-		ExitOnDebugSuccess: false,
-		PrintFunc: func(format string, data ...interface{}) {
-			log.Error().Msgf(format, data...)
-		},
-	})
-
-	if zerolog.GlobalLevel() <= zerolog.TraceLevel {
-		//adds file and line number to log
-		log.Logger = log.With().Caller().Logger()
-	} else {
-		errorLogger.Disable(true)
-	}
-
-	DebugError = errorLogger.Debug
 }
 
 func LoadPicture(path string) (pixel.Picture, error) {
